a945eb19262d406b30c0b1fc5b58050ffc69e6ee
Gabriel Araujo <gbmsaraujo@gmail.com>
2025-07-16 13:37:20 -0300

    studying go basics

---
+++ basics/variables.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+var middleName = "Santos"
+
+func main() {
+	const PI = 3.14 
+	// var age int
+	// var name = "John"
+
+	// count := 10 
+	// lastName := "Smith"
+
+	middleName = "Araujo"
+
+	fmt.Println(middleName)
+	
+
+	
+
+}
+
+func printName() {
+	firstName := "Gabriel"
+	fmt.Println(firstName)
+}
